98180807c7f1f1a3841ff036d8115c3ed96acfdd
ie-katerinai <117760910+ie-katerinai@users.noreply.github.com>
2023-02-14 00:54:11 +0300

    Add L1

---
+++ Lectures/Lecture_1/Lecture_1.py
@@ -0,0 +1,9 @@
+username = input('Введите имя: ')
+if username == 'Маша':
+    print('ура, это МАША!')
+elif username == 'Марина':
+    print('Я вас так ждал, Марина')
+elif username == 'Ильнар':
+    print('Ильнар - топ)')
+else:
+    print('Привет, ', username)
