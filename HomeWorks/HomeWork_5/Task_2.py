94c8dbfc78578beff80816a52c5eb2e80758f082
ie-katerinai <117760910+ie-katerinai@users.noreply.github.com>
2023-02-14 20:41:38 +0300

    Modif HW 5 Task 2

--- HomeWorks/HomeWork_5/Task_2.py
+++ HomeWorks/HomeWork_5/Task_2.py
@@ -3,23 +3,17 @@
 # 2 2
 # 4
 
-def summa(numb_1, numb_2):
-    summ = 1
-    if numb_1 == 0 and numb_2 == 0:
-        return 0
-    elif numb_2 == 0 and numb_1 != 0:
-            numb_1 -= 1
-            summ +=1
-            return summ
-    elif numb_1 == 0 and numb_2 != 0:
-        numb_2 -= 1
-        summ +=1
-        return summ
-    else: summa(numb_1 - 1, numb_2) 
+def summa(numb, count):
+    if numb == 0:
+        return count
+    else: count = 1 + summa(numb - 1, count)
+    return count
 
 a = int(input('Введите число a: '))
 b = int(input('Введите число b: '))
-print(summa(a, b))
+count = 0
+s = summa(a, count) + summa(b, count)
+print(s)
 
 
 
